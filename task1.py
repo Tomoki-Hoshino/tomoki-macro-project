4cfcc2c600ae14fbea04f6a2dcb6f3efcbe2d8dd
Tomoki-Hoshino <tomokihoshino0607@keio.jp>
2025-05-13 12:39:02 +0900

    0513

--- task1.py
+++ task1.py
@@ -10,11 +10,20 @@ end_date = '2024-10-01'
 bgdp = DataReader('NGDPRSAXDCBRQ','fred',start_date,end_date)
 log_gdp = np.log(bgdp)
 
-cycle, trend = sm.tsa.filters.hpfilter(log_gdp, lamb=1600)
+lambdas = [10,100,1600]
+trends = {}
+
+for lam in lambdas:
+    _,trend = sm.tsa.filters.hpfilter(log_gdp, lamb=lam)
+    trends[lam] = trend
 
 plt.figure(figsize=(12, 6))
-plt.plot(log_gdp, label="Log of Real GDP", color='blue')
-plt.plot(trend, label="HP Filter Trend", color='red', linestyle='--')
+plt.plot(log_gdp, label="Log of Real GDP", color='black')
+
+colors = ['red','green','blue']
+for lam, colar in zip(lambdas,  colors):
+    plt.plot(trends[lam], label=f"HP Trend (λ={lam})", linestyle='--', color=color)
+
 plt.title("Log of Brazil Real GDP and HP Trend (1996–2024)")
 plt.xlabel("Date")
 plt.ylabel("Log GDP")
